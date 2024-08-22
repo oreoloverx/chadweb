5cfd1f4d50751bc0aad50ce82f995ad82e25dbb5
Matthew Samushonok <matthew.samushonok@gmail.com>
2024-08-22 17:00:22 +0300

    asdasd

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import chadDesc from "./assets/cocaine.png";
 import dex from "./assets/dex.png";
 import tw from "./assets/tw.png";
 import tg from "./assets/tg.png";
-import chad from "./assets/mainchad.png"
+import chad from "./assets/mainchad.png";
+import boat from "./assets/boat.png";
 
 function App() {
   let ca = "-TBA-";
@@ -21,13 +22,34 @@ function App() {
             <p className="caText">CA: {ca}</p>
           </div>
           <div className="socialBlock">
-            <img className="social" src={tg} />
-            <img className="social" src={tw} />
-            <img className="social" src={dex} />
+            <a href="https://t.me/_" target="_blank" rel="noopener noreferrer"><img className="social" src={tg} /></a>
+            <a href="https://t.me/_" target="_blank" rel="noopener noreferrer"><img className="social" src={tw} /></a>
+            <a href="https://t.me/_" target="_blank" rel="noopener noreferrer"><img className="social" src={dex} /></a>
           </div>
           <img className="chad" alt="CHAD" src={chad}/>
         </div>
       </div>
+      <div className="section2">
+          <div className="section2-block">
+            <div className="s2b-left">
+              <h2 className="heading1">Utility: Farming Rewards Forever</h2>
+              <p className="text">
+                Top $CHAD holders farm massive TX fees—stack your bags and watch the rewards flow nonstop. The dev is all-in, reinvesting his 20% cut to keep the cartel growing stronger. Get in, hold tight, and let the narco profits roll in forever (DYOR: https://docs.moonshot.cc/faq)
+              </p>
+            </div>
+            <div className="s2b-right">
+              <img className="boat" alt="ChadsBoat" src={boat} />
+            </div>
+          </div>
+        </div>
+        <div className="section3">
+          <p className="divider"></p>
+        </div>
+        <div className="section4">
+          <div className="">
+          <h2 className="heading">Snort, Flip, Repeat.</h2>
+          <p className="">
+        </div>
     </div>
   );
 }

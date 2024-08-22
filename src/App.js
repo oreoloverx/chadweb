cd63006994e557dc81eb2210acc5ed08ae24d142
Matthew Samushonok <matthew.samushonok@gmail.com>
2024-08-22 16:54:51 +0300

    asdasdasd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from "./assets/logo.png";
+import chadText from "./assets/CHAD.png";
+import chadDesc from "./assets/cocaine.png";
+import dex from "./assets/dex.png";
+import tw from "./assets/tw.png";
+import tg from "./assets/tg.png";
+import chad from "./assets/mainchad.png"
 
 function App() {
+  let ca = "-TBA-";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="section1">
+        <div className="section1-block">
+          <img className="logo" alt="logoChad" src={logo}/>
+          <img className="chadText" alt="$CHAD" src={chadText}/>
+          <img className="chadDesc" alt="Cocaine High And Dominant" src={chadDesc}/>
+          <div className="cabox">
+            <p className="caText">CA: {ca}</p>
+          </div>
+          <div className="socialBlock">
+            <img className="social" src={tg} />
+            <img className="social" src={tw} />
+            <img className="social" src={dex} />
+          </div>
+          <img className="chad" alt="CHAD" src={chad}/>
+        </div>
+      </div>
     </div>
   );
 }

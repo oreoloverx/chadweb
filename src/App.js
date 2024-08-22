f54fd90d1072f7c1bfd914be3be050f0c1562c60
Matthew Samushonok <matthew.samushonok@gmail.com>
2024-08-22 20:05:02 +0300

    moasdasd

--- src/App.js
+++ src/App.js
@@ -1,15 +1,4 @@
 import './App.css';
-import logo from "./assets/logo.png";
-import chadText from "./assets/CHAD.png";
-import chadDesc from "./assets/cocaine.png";
-import chadMob from "./assets/cocainemob.png";
-import dex from "./assets/dex.svg";
-import tw from "./assets/tw.svg";
-import tg from "./assets/tg.svg";
-import chad from "./assets/mainchad.png";
-import boat from "./assets/boat.png";
-import sound from './assets/music/audio.mp3';
-import Overlay from './components/Overlay';
 import Coming from './components/Coming';
 
 function App() {
